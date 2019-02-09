cfdc33655a65715977cf5ad8a138e5ae43c0a052
Patrick <paichun.wang@yahoo.com>
2019-02-09 15:58:55 -0600

    added mongodb localhost, checking to see if connect to mlab

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const path = require("path");
 const generatePassword = require("password-generator");
-
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(
+    () => {
+      console.log("Database is connected");
+    },
+    err => {
+      console.log("Can not connect to the database" + err);
+    }
+  )
+  .catch(err => {
+    console.log("Error on start: " + err.stack);
+    process.exit(1);
+  });
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Put all API endpoints under '/api'
 app.get("/api/passwords", (req, res) => {
@@ -28,7 +52,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
+app.listen(PORT, () => {
+  console.log("Server is running on Port: ", PORT);
+});
