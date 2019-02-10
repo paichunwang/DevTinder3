5e06e9117769927ef85812fbd373d637cfe8335f
Patrick <paichun.wang@yahoo.com>
2019-02-10 00:02:51 -0600

    heroku deploy works, getting routes folder backup

--- server.js
+++ server.js
@@ -4,22 +4,10 @@ const generatePassword = require("password-generator");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Put all API endpoints under '/api'
-app.get("/api/passwords", (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  );
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
+const router = require("./routes/routes");
 
 app.use(express.static(path.join(__dirname, "client/build")));
+app.use(router);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
@@ -28,10 +16,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
-});
 
 app.listen(port, (req, res) => {
   console.log(`server listening on port: ${port}`);
