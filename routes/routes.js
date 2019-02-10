84b54a31a7fbd75fe5d2ecade18f0cc73a16e712
Patrick <paichun.wang@yahoo.com>
2019-02-09 22:06:02 -0600

    connected routes, checking on heroku for deployment

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const generatePassword = require("password-generator");
+const path = require("path");
+
+app.get("/api/passwords", (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  );
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,56 +1,38 @@
 const express = require("express");
-const path = require("path");
-const generatePassword = require("password-generator");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
-const cors = require("cors");
+// const cors = require("cors");
+
+const router = require("./routes/routes");
 
 require("dotenv").config();
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(
-    () => {
-      console.log("Database is connected");
-    },
-    err => {
-      console.log("Can not connect to the database" + err);
-    }
-  )
-  .catch(err => {
-    console.log("Error on start: " + err.stack);
-    process.exit(1);
-  });
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(
+//     () => {
+//       console.log("Database is connected");
+//     },
+//     err => {
+//       console.log("Can not connect to the database" + err);
+//     }
+//   )
+//   .catch(err => {
+//     console.log("Error on start: " + err.stack);
+//     process.exit(1);
+//   });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
-app.use(cors());
+//this usess custom router in routes/routes.js
+app.use(router);
+
+//body parser for decoding https request
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Put all API endpoints under '/api'
-app.get("/api/passwords", (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  );
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log("Server is running on Port: ", PORT);
