e927cb245ac1406fdd636ac7d8c5506e246c4ba0
Patrick <paichun.wang@yahoo.com>
2019-02-09 23:51:19 -0600

    reverting

--- server.js
+++ server.js
@@ -1,34 +1,9 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5000;
-// const cors = require("cors");
 const path = require("path");
+const generatePassword = require("password-generator");
+const app = express();
 
-// const router = require("./routes/routes");
-
-require("dotenv").config();
-
-// mongoose
-//   .connect(process.env.MONGODB_URI)
-//   .then(
-//     () => {
-//       console.log("Database is connected");
-//     },
-//     err => {
-//       console.log("Can not connect to the database" + err);
-//     }
-//   )
-//   .catch(err => {
-//     console.log("Error on start: " + err.stack);
-//     process.exit(1);
-//   });
-
-//this uses custom router in routes/routes.js
-// app.use("/", express.static("public"));
-// app.use(router);
-
+// Put all API endpoints under '/api'
 app.get("/api/passwords", (req, res) => {
   const count = 5;
 
@@ -43,15 +18,13 @@ app.get("/api/passwords", (req, res) => {
   console.log(`Sent ${count} passwords`);
 });
 
-//catch all non-existing routes and serve the react static files
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-//body parser for decoding https request
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-app.listen(PORT, () => {
-  console.log("Server is running on Port: ", PORT);
-});
+console.log(`Password generator listening on ${port}`);
