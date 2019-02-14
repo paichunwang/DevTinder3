707eb35f7029c6b4b2bd444efa42f4baf8889a13
Patrick <paichun.wang@yahoo.com>
2019-02-14 15:52:32 -0600

    added material ui theme option instead of external css

--- client/src/components/landingpage/nav_gettingstarted/navbar.jsx
+++ client/src/components/landingpage/nav_gettingstarted/navbar.jsx
@@ -9,57 +9,20 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import LoginIcon from "@material-ui/icons/Input";
 import RegsIcon from "@material-ui/icons/HowToReg";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 // import { Link } from "react-router-dom";
 
-import "./navbar.css";
-// import { withRouter } from "react-router-dom";
-
-class Navbar extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" className={classes.grow}>
-              DevTinder
-            </Typography>
-            <Button
-              // component={Link}
-              // to="/login"
-              href="/login"
-              state="hello"
-              color="inherit"
-              className={classes.button}
-            >
-              Login
-              <LoginIcon className={classes.rightIcon} />
-            </Button>
-            <Button
-              // component={Link}
-              // to="/login"
-              href="/signup"
-              onClick={this.handleSignup}
-              color="inherit"
-              className={classes.button}
-            >
-              Signup
-              <RegsIcon className={classes.rightIcon} />
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "rgba(0, 0, 0, 0.5)"
+    },
+    secondary: {
+      main: "#FFFFFF"
+    }
   }
-}
+});
 
 const styles = {
   root: {
@@ -74,6 +37,57 @@ const styles = {
   }
 };
 
+class Navbar extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <AppBar position="absolute" color="primary">
+            <Toolbar>
+              <IconButton
+                className={classes.menuButton}
+                color="secondary"
+                aria-label="Menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                color="secondary"
+                className={classes.grow}
+              >
+                DevTinder
+              </Typography>
+              <Button
+                // component={Link}
+                // to="/login"
+                href="/login"
+                state="hello"
+                color="secondary"
+                className={classes.button}
+              >
+                Login
+                <LoginIcon className={classes.rightIcon} />
+              </Button>
+              <Button
+                // component={Link}
+                // to="/login"
+                href="/signup"
+                onClick={this.handleSignup}
+                color="secondary"
+                className={classes.button}
+              >
+                Signup
+                <RegsIcon className={classes.rightIcon} />
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
 // function ButtonAppBar(props) {
 //   const { classes } = props;
 //   return (
