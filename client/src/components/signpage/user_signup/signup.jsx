f41917058655ae2e109a48da631eb1a57e436467
Patrick <paichun.wang@yahoo.com>
2019-02-15 12:26:04 -0600

    added inline css for signup page, with dynamic rendering using object

--- client/src/components/signpage/user_signup/signup.jsx
+++ client/src/components/signpage/user_signup/signup.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import { withStyles, MuiThemeProvider } from "@material-ui/core/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import green from "@material-ui/core/colors/green";
 
 const theme = createMuiTheme({
   palette: {
-    primary: green
+    primary: { 500: "#3498db" },
+    error: { 500: "#FFFFFF" }
   },
   typography: { useNextVariants: true }
 });
@@ -22,29 +22,41 @@ const styles = theme => ({
   }
 });
 
-class Signup extends Component {
-  state = {
-    fields: [
-      {
-        fname: "First Name",
-        lname: "Last Name",
-        email: "Email",
-        password: "Password (6 or more character)"
-      }
-    ]
-  };
+const Values = {
+  fname: "First Name",
+  lname: "Last Name",
+  email: "Email",
+  password: "Password (6 or more character)"
+};
 
+const columnStyle = {
+  width: "30%"
+  // background-color: #ffffff60;
+  // margin-top: 20vh;
+  // padding: 50px;
+  // border-radius: 15px;
+};
+
+class Signup extends Component {
   render() {
-    const { classes } = this.props;
     return (
-      <div className={classes.root}>
-        <MuiThemeProvider theme={theme}>
-          <TextField
-            className={classes.margin}
-            label="MuiThemeProvider"
-            id="mui-theme-provider-standard-input"
-          />
-        </MuiThemeProvider>
+      <div className="column" style={columnStyle}>
+        <div>
+          <MuiThemeProvider theme={theme}>
+            {Object.keys(Values).map((keyName, keyIndex) => {
+              return (
+                <div>
+                  <TextField
+                    className={keyName}
+                    label={Values[keyName]}
+                    id="mui-theme-provider-standard-input"
+                    fullWidth="true"
+                  />
+                </div>
+              );
+            })}
+          </MuiThemeProvider>
+        </div>
       </div>
     );
   }
